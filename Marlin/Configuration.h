4ba36cd39a17b80de626064ab60fd4a23918d1a0
Zoller Thierry <thierry@zoller.lu>
2020-08-02 16:50:53 +0200

    as
    as

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -138,15 +138,13 @@
   //#define INVERT_E1
 #endif
 
-<<<<<<< HEAD
+
 // CoolStep. Currently supported for TMC2130, TMC2209, TMC5130 and TMC5160 only.
 // This mode allows for cooler steppers and energy savings.
 // the driver will switch to coolStep when stepper speed is over COOLSTEP_THRESHOLD mm/s.
 // Settings for CoolStep in CONFIGURATION_ADV.h on Line 2499 
-#define COOLSTEP
+//#define COOLSTEP
 
-=======
->>>>>>> parent of 232ca06... Make COOLSTEP as an OPTION not Enabled by DEFAULT
 // Custom Axis Steps Per MM
 // If you have calibrated the extruder before, you can enter the steps here, also be specified individually for the other axes.
 //#define STEPS_X         0  // Normally no change needed...
